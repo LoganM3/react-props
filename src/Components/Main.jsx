9691826a5260c852e16baf5acecbdc4e7eac8d8e
Logan McCalley <mccalleylogan@gmail.com>
2022-08-08 13:53:25 -0400

    react props

---
+++ src/Components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+ function Main(){
+
+    return(
+        <main>
+            <h1>Howdy</h1>
+        </main>
+    )
+}
+
+export default Main;
---
+++ src/Components/Menubar.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+function Menubar(){
+    return (
+        <nav>
+            menu bar
+        </nav>
+
+
+    )
+}
+
+
+export default Menubar;
