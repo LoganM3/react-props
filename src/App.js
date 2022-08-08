f91c60e13824ebe61ef72a78ec1b1c371bf6708d
Logan McCalley <mccalleylogan@gmail.com>
2022-08-08 16:13:10 -0400

    added coffee component;get data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
 import Menubar from './Components/Menubar';
 import Main from './Components/Main';
+import Coffee from './Components/Coffee';
 import Footer from './Components/Footer';
 import './App.css';
+
 
 function App() {
   const [name,setName] = useState('Mason')
@@ -11,6 +13,7 @@ function App() {
     
 <Menubar name={name} setName ={setName}/>
 <Main name={name}/>
+<Coffee />
 <Footer/>
 
     </div>
